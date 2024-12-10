b650fae4c7677a346a32759a8159b86813030d34
Angeliázer Rigon da Silva <yyrigon@hotmail.com>
2024-12-10 12:40:29 -0300

    First Commit

--- src/instance-axios.js
+++ src/instance-axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    // baseURL: import.meta.env.VITE_APP_BASE_URL,
-    baseURL: 'https://apinode-production-b096.up.railway.app',
+    baseURL: import.meta.env.VITE_APP_BASE_URL,
+    //baseURL: 'https://apinode-production-b096.up.railway.app',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
